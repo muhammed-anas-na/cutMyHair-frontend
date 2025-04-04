02666eec1bec1c749b5a8f7bcd35e94c142d0075
Muhammed Anas NA <anasna6005@gmail.com>
2025-04-04 21:14:04 +0530

    Backend url changed

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://cutmyhair-backend.onrender.com/api';
 const OWNER_BASE_URL = `${BASE_URL}/owner`;
 const USER_BASE_URL = `${BASE_URL}/users`;
 const BLOG_BASE_URL = `${BASE_URL}/blogs`;
